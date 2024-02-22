36fd85d84928280953e9e169b063613445f09505
DenDhim4s <dhimaswakhid201705@gmail.com>
2024-02-22 10:58:54 +0700

    update login

--- app/src/main/java/com/example/dntylancar/RegisterActivity.kt
+++ app/src/main/java/com/example/dntylancar/RegisterActivity.kt
@@ -1,11 +1,18 @@
 package com.example.dntylancar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val loginLink = findViewById<TextView>(R.id.login2)
+        loginLink.setOnClickListener(){
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+        }
     }
 }
