54fd2cb7808262a52518745a70db7192cdb23f08
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2024-02-22 10:48:01 +0700

    FA - Create RegisterActivity.kt - Update activity_login.xml + activity_register.xml + AndroidManifest.xml + LoginActivity.kt - Delete

--- app/src/main/java/com/example/dntylancar/LoginActivity.kt
+++ app/src/main/java/com/example/dntylancar/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.dntylancar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registerLink = findViewById<TextView>(R.id.RegisterLinkText)
+        registerLink.setOnClickListener(){
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
     }
 }
