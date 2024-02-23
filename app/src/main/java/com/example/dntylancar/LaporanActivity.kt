64d33849fbace936731cda35b10a01a6893d3e1b
DenDhim4s <dhimaswakhid201705@gmail.com>
2024-02-23 09:36:51 +0700

    update pop up report

--- app/src/main/java/com/example/dntylancar/LaporanActivity.kt
+++ app/src/main/java/com/example/dntylancar/LaporanActivity.kt
@@ -1,15 +1,35 @@
 package com.example.dntylancar
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import com.example.dntylancar.databinding.ActivityLaporanBinding
+import com.example.dntylancar.databinding.ActivityPopUpReportBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class LaporanActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLaporanBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_laporan)
+        setContentView(binding.root)
 
-        
+        binding.btnAddReport.setOnClickListener {
+            showBottomSheet()
+        }
     }
+
+    private fun showBottomSheet(){
+        val sheetDialog = BottomSheetDialog(this)
+        val sheetBinding = ActivityPopUpReportBinding.inflate(layoutInflater)
+        sheetDialog.apply {
+            setContentView(sheetBinding.root)
+            show()
+        }
+
+        sheetBinding.btnSend.setOnClickListener{
+            sheetDialog.dismiss()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/dntylancar/PopUpReportActivity.kt
@@ -0,0 +1,15 @@
+package com.example.dntylancar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class PopUpReportActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
